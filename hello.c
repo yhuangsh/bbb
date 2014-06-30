bd76deacb7f5d21549f1992fbd5ac5f1378a2d11
Yong Huang <yong.huang.sh@gmail.com>
2014-06-30 21:31:41 +0800

    save work

--- hello.c
+++ hello.c
@@ -1,84 +1,143 @@
 typedef unsigned char u1;
 typedef unsigned int u4;
 
-union u4bits_t
+struct u4bits_t
 {
-  u4 theu4;
-  struct 
-  {
-    unsigned b0:1;
-    unsigned b1:1;
-    unsigned b2:1;
-    unsigned b3:1;
-    unsigned b4:1;
-    unsigned b5:1;
-    unsigned b6:1;
-    unsigned b7:1;
-    unsigned b8:1;
-    unsigned b9:1;
-    unsigned b10:1;
-    unsigned b11:1;
-    unsigned b12:1;
-    unsigned b13:1;
-    unsigned b14:1;
-    unsigned b15:1;
-    unsigned b16:1;
-    unsigned b17:1;
-    unsigned b18:1; 
-    unsigned b19:1;
-    unsigned b20:1;
-    unsigned b21:1;
-    unsigned b22:1;
-    unsigned b23:1;
-    unsigned b24:1;
-    unsigned b25:1;
-    unsigned b26:1;
-    unsigned b27:1;
-    unsigned b28:1;
-    unsigned b29:1;
-    unsigned b30:1;
-    unsigned b31:1;
-  } bits;
+  unsigned b0:1;
+  unsigned b1:1;
+  unsigned b2:1;
+  unsigned b3:1;
+  unsigned b4:1;
+  unsigned b5:1;
+  unsigned b6:1;
+  unsigned b7:1;
+  unsigned b8:1;
+  unsigned b9:1;
+  unsigned b10:1;
+  unsigned b11:1;
+  unsigned b12:1;
+  unsigned b13:1;
+  unsigned b14:1;
+  unsigned b15:1;
+  unsigned b16:1;
+  unsigned b17:1;
+  unsigned b18:1; 
+  unsigned b19:1;
+  unsigned b20:1;
+  unsigned b21:1;
+  unsigned b22:1;
+  unsigned b23:1;
+  unsigned b24:1;
+  unsigned b25:1;
+  unsigned b26:1;
+  unsigned b27:1;
+  unsigned b28:1;
+  unsigned b29:1;
+  unsigned b30:1;
+  unsigned b31:1;
 };
     
 struct gpio_t 
 {
   struct 
   {
-    unsigned minor:6;
-    unsigned custom:2;
-    unsigned major:3;
-    unsigned rtl:5;
-    unsigned func:12;
-    unsigned reserved:2;
-    unsigned scheme:2;
-  } revision;
-  u4 reserved0[3];
-  struct 
-  {
-    unsigned autoidle:1;
-    unsigned softreset:1;
-    unsigned enawakeup:1;
-    unsigned idlemode:2;
-    unsigned reserved:27;
-  } sysconfig;
-  u4 reserved1[3];
+    struct 
+    {
+      unsigned minor:6;
+      unsigned custom:2;
+      unsigned major:3;
+      unsigned rtl:5;
+      unsigned func:12;
+      unsigned reserved:2;
+      unsigned scheme:2;
+    } revision;
+  } off0x00;
+
+  u4 off0x04;
+  u4 off0x08;
+  u4 off0x0c;
+
   struct
   {
-    unsigned dmaevent_ack:1;
-    unsigned reserved:31;
-  } eoi;
-  union u4bits_t irqstatus_raw_0;
-  union u4bits_t irqstatus_raw_1;
-  union u4bits_t irqstatus_0;
-  union u4bits_t irqstatus_1;
-  union u4bits_t irqstatus_set_0;
-  union u4bits_t irqstatus_set_1;
-  union u4bits_t irqstatus_clr_0;
-  union u4bits_t irqstatus_clr_1;
-  union u4bits_t irqwaken_0;
-  union u4bits_t irqwaken_1;
-  u4 reserved2[50];
+    struct
+    {
+      unsigned autoidle:1;
+      unsigned softreset:1;
+      unsigned enawakeup:1;
+      unsigned idlemode:2;
+      unsigned reserved:27;
+    } sysconfig;
+  } off0x10;
+  
+  u4 off0x14;
+  u4 off0x18;
+  u4 off0x1c;
+
+  struct
+  {
+    struct
+    {
+      unsigned dmaevent_ack:1;
+      unsigned reserved:31;
+    } eoi;
+  } off0x20;
+
+  struct
+  {
+    struct u4bits_t irqstatus_raw_0;
+  } off0x24;
+  struct
+  {
+    struct u4bits_t irqstatus_raw_1;
+  } off0x28;
+  struct
+  {
+    struct u4bits_t irqstatus_0;
+  } off0x2c;
+  struct
+  {
+    struct u4bits_t irqstatus_1;
+  } off0x30;
+  struct
+  {
+    struct u4bits_t irqstatus_set_0;
+  } off0x34;
+  struct
+  {
+    struct u4bits_t irqstatus_set_1;
+  } off0x38;
+  struct
+  {
+    struct u4bits_t irqstatus_clr_0;
+  } off0x3c;
+  struct
+  {
+    struct u4bits_t irqstatus_clr_1;
+  } off0x40;
+  struct
+  {
+    struct u4bits_t irqwaken_0;
+  } off0x44;
+  struct
+  {
+    struct u4bits_t irqwaken_1;
+  } off0x48;
+  
+  u4 off0x4c;
+
+  u4 off0x50;
+  u4 off0x54;
+  u4 off0x58;
+  u4 off0x5c;
+
+  u4 off0x60;
+  u4 off0x64;
+  u4 off0x68;
+  u4 off0x6c;
+
+  u4 off0x70;
+  u4 off0x74;
+  
   struct
   { 
     unsigned resetdone:1;
@@ -517,6 +576,8 @@ struct uart_t
     } eblr;
   } off0x48;
 
+  u4 off0x4c;
+
   union
   {
     u4 reserved;
@@ -551,6 +612,109 @@ struct uart_t
     } syss;
   } off0x58;
 
+  union 
+  {
+    u4 reserved;
+    struct
+    {
+      unsigned cts_activity:1;
+      unsigned dsr_activity:1;
+      unsigned ri_activity:1;
+      unsigned dcd_activity:1;
+      unsigned rx_activity:1;
+      unsigned rhr_interrupt:1;
+      unsigned rls_interrupt:1;
+      unsigned txwakeupen:1;
+      unsigned reserved;
+    } wer;
+  } off0x5c;
+  
+  union 
+  {
+    u4 reserved;
+    struct
+    {
+      u1 cfps;
+      u1 reserved;
+    } cfps;
+  } off0x60;
+
+  union
+  {
+    u4 reserved;
+    struct
+    {
+      u1 rxfifo_lvl;
+      u1 reserved;
+    } rxfifo_lvl;
+  } off0x64;
+
+  union
+  {
+    u4 reserved;
+    struct
+    {
+      u1 txfifo_lvl;
+      u1 reserved;
+    } txfifo_lvl;
+  } off0x68;
+
+  union
+  {
+    u4 reserved;
+    struct
+    {
+      unsigned en_rxfifo_empty:1;
+      unsigned en_txfifo_empty:1;
+      unsigned reserved:14;
+    } ier2;
+  } off0x6c;
+
+  union
+  {
+    u4 reserved;
+    struct
+    {
+      unsigned rxfifo_empty_sts:1;
+      unsigned txfifo_empty_sts:1;
+      unsigned reserved:14;
+    } isr2;
+  } off0x70;
+
+  union
+  {
+    u4 reserved;
+    struct
+    {
+      u1 freq_sel;
+      u1 reserved;
+    } freq_sel;
+  } off0x74;
+
+  u4 off0x78;
+  u4 off0x7c;
+  
+  union
+  {
+    u4 reserved;
+    struct
+    {
+      unsigned disable_cir_rx_demod:1;
+      unsigned nondefault_freq:1;
+      unsigned set_dma_tx_threshold:1;
+      unsigned reserved:13;
+    } mdr3;
+  } off0x80;
+
+  union
+  {
+    u4 reserved;
+    struct
+    {
+      unsigned tx_dma_threshold:6;
+      unsigned reserved:10;
+    } tx_dma_threshold;
+  } off0x84;
 
 };
 
